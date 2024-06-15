30e0c408e260ed78de77661828358505b86b226a
Ahmad Syaifuddin <ahmadsyai598@gmail.com>
2024-06-15 14:57:49 +0800

    SELESAI TUNTAS WEBGIS

--- _halaman/beranda.php
+++ _halaman/beranda.php
@@ -1,7 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="./assets/css/beranda.css">
+</head>
+<body>
 <?php
   $title="Beranda";
   $judul=$title;
 ?>
-<?=content_open('Halaman Beranda')?>
+<?=content_open('<strong>Halaman Beranda WebGIS Pangan Kabupaten Barito Kuala 🌾</strong>')?>
     <?=$session->pull("info")?>
-<?=content_close()?>
+<?=content_close()?> 
+</body>
+</html>
+
+
--- _halaman/login_v1.php
+++ _halaman/login_v1.php
@@ -38,7 +38,7 @@
 <body class="hold-transition login-page">
 <div class="login-box">
   <div class="login-logo">
-    <a href="#"><b>Login</b>WEBGIS Pangan Unggulan Batola</a>
+    <a href="#"><b>Login</b>WEBGIS Pangan Unggulan Batola 🌾</a>
   </div>
   <!-- /.login-logo -->
   <div class="login-box-body">
